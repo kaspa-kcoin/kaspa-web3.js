42965a090d09d045f13589fde6f9da31dbf3c638
Victor <kaspakoin@gmail.com>
2024-12-31 13:43:20 +0800

    fix: krc20 export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './tx';
 export * from './tx-script';
 export * from './address';
 export * from './consensus';
+export * from './krc20';
 export { Keypair } from './keypair';
 export { kaspaToSompi, addressFromScriptPublicKey } from './utils';
 export { SendKasParams, SendKrc20Params } from './send-param';
